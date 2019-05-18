55578023d315630de968b2cc7ad7a7bccb32c74b
Dell <olgaigpavlenko@gmail.com>
2019-05-18 10:27:46 +0300

    Figures are .added

---
+++ Education/Figure_Polimorphysm/Triangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure_Polimorphysm
+{
+    public class Triangle : Figure
+    {
+        private double height;
+        private double baseLength;
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    height = value;
+                }
+            }
+        }
+
+        public double BaseLength
+        {
+            get
+            {
+                return baseLength;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    baseLength = value;
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine($"Drawed triangle with height: {height} and baselength: {baseLength}");
+        }
+
+        public Triangle(int X, int Y, double height, double baseLength)
+            : base(X, Y)
+        {
+            this.Height = height;
+            this.baseLength = baseLength;
+        }
+    }
+}

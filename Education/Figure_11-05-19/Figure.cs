dcd2636924f5ae5b87fdeb7ba11f2eec586401fe
Dell <olgaigpavlenko@gmail.com>
2019-05-18 10:16:20 +0300

    Figures are aded.

---
+++ Education/Figure_11-05-19/Figure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure_11_05_19
+{
+    public class Figure
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Figure(double X, double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+}
---
+++ Education/Figure_11-05-19/Square.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure_11_05_19
+{
+    public class Square : Figure
+    {
+        private int sideLength;
+
+        public int SideLength
+        {
+            get
+            {
+                return sideLength;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    sideLength = value;
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine($"Drawed square with sideLength: {sideLength}");
+        }
+
+        public Square (int X, int Y, int sideLength)
+            :base(X, Y)
+        {
+            this.SideLength = sideLength;
+        }
+    }
+}

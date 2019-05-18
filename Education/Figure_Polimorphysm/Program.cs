046863a80268fe8c3b8e329aad110a4d3af62383
Dell <olgaigpavlenko@gmail.com>
2019-05-18 10:24:00 +0300

    Project is created.

---
+++ Education/Figure_Polimorphysm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure_Polimorphysm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

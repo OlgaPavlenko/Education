699f76b412b6aeaa605ee5d13e944f9cd0f7313b
Dell <olgaigpavlenko@gmail.com>
2019-06-08 13:50:03 +0300

    added number of folders

---
+++ Education/PrscticeJune8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PrscticeJune8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Windows\System32";
+            var directoryInfo = new DirectoryInfo(path);
+            int count = 0;
+            foreach (var item in directoryInfo.GetDirectories())
+            {
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}

3a0ba1ce9f3245eae6cc0f91ed6fab172320daac
Olga Pavlenko <olgaigpavlenko@gmail.com>
2019-07-03 14:40:37 +0300

    Added MyList

---
+++ Education/MyList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>();
+            list.Add(5);
+            list.Add(10);
+            list.Add(8);
+            list.Add(3);
+            list.Add(2);
+            list.Add(0);
+            list.Contains(8);
+            Console.WriteLine(list);
+            list.Clear();
+        }
+    }
+}

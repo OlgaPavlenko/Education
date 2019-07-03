d376da7c5a6e2e12d9afb6af86b988af4b3560ea
Olga Pavlenko <olgaigpavlenko@gmail.com>
2019-07-03 14:00:46 +0300

    added Pupil

---
+++ Education/Pupil/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pupil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pupil[] pupils = new Pupil[2];
+
+            pupils[0] = new Pupil("Rodion", new DateTime(2013, 05, 31), "Pavlenko", "53", "1A");
+            pupils[1] = new Pupil("Vita", new DateTime(2010, 02, 23), "Gugug", "153", "3B");
+
+            Console.WriteLine("\n" + String.Format("{0, -10} {1, -10} {2, -10} {3, -5} {4, -8} {5, -7}", "Name", "Surname", "BirthYear", "Age", "School", "Class"));
+            foreach (var item in pupils)
+            {
+                Console.WriteLine(String.Format("{0, -10} {1, -10} {2, -10} {3, -5} {4, -8} {5, -7}", item.name, item.secondName, item.yearOfBirth, item.age, item.School, item.SchoolClass));
+            }
+        }
+    }
+}
---
+++ Education/Pupil/Pupil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pupil
+{
+    class Pupil
+    {
+
+        public string name;
+        public string secondName;
+        public string School { get; set; }
+        public string SchoolClass { get; set; }
+        public int age;
+        public int yearOfBirth;
+
+        public int GetAge(DateTime dateOfBirth)
+        {
+            int age;
+            age = Convert.ToInt32(DateTime.Now.Year - dateOfBirth.Year);
+            return age;
+        }
+
+        public Pupil(string name, DateTime dateOfBirth, string secondName, string School, string SchoolClass)
+        {
+            this.yearOfBirth = dateOfBirth.Year;
+            this.age = GetAge(dateOfBirth);
+            this.name = name;
+            this.secondName = secondName;
+            this.School = School;
+            this.SchoolClass = SchoolClass;
+        }
+    }
+}

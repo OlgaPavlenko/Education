ab45b716af190fc582c1962870800745525daa06
Dell <olgaigpavlenko@gmail.com>
2019-07-13 12:38:33 +0300

    added PingPongEventArgs

---
+++ Education/PingPong/Ping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong
+{
+    class Ping
+    {
+        public event EventHandler NewPing;
+        protected virtual void OnNewPing(EventArgs e)
+        {
+            this.NewPing?.Invoke(this, e);
+        }
+        public void MadePing()
+        {
+            Console.WriteLine("I Made ping");
+            System.Threading.Thread.Sleep(1000);
+            this.OnNewPing(new EventArgs());
+        }
+    }
+}
---
+++ Education/PingPong/Pong.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong
+{
+    public class Pong
+    {
+        public event EventHandler NewPong;
+        protected virtual void OnNewPong(EventArgs e)
+        {
+            this.NewPong?.Invoke(this, e);
+        }
+        public void MadePong()
+        {
+            Console.WriteLine("I Made pong");
+            System.Threading.Thread.Sleep(1000);
+            this.OnNewPong(new EventArgs());
+        }
+    }
+}
+

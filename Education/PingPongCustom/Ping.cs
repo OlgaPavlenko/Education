697819f77f31eb6d9b9b2d38d10908ca0cc7f00b
Dell <olgaigpavlenko@gmail.com>
2019-07-13 13:15:55 +0300

    added PingPongCustom

---
+++ Education/PingPongCustom/Ping.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongCustom
+{
+    class Ping
+    {
+        public event PingPongDelegate NewPing;
+
+        protected virtual void OnNewPing(PingPongEventArgs e)
+        {
+            this.NewPing?.Invoke(e);
+        }
+
+        public void MadePing()
+        {
+            Console.WriteLine("I Made ping");
+            System.Threading.Thread.Sleep(1000);
+            this.OnNewPing(new PingPongEventArgs(DateTime.Now));
+        }
+    }
+}
---
+++ Education/PingPongCustom/Pong.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongCustom
+{
+    public class Pong
+    {
+        public event PingPongDelegate NewPong;
+        protected virtual void OnNewPong(PingPongEventArgs e)
+        {
+            this.NewPong?.Invoke(e);
+        }
+
+        public void MadePong()
+        {
+            Console.WriteLine("I Made pong");
+            System.Threading.Thread.Sleep(1000);
+            this.OnNewPong(new PingPongEventArgs(DateTime.Now));
+        }
+    }
+}

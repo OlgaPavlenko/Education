608c4967a7b9902d79030e90ee8f4f8a14d24299
Olga Pavlenko <olgaigpavlenko@gmail.com>
2019-07-15 19:52:00 +0300

    added Queue2

---
+++ Education/Queue2/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue2
+{
+    class Book
+    {
+        public string Name { get; set; }
+
+        public Book(string name)
+        {
+            this.Name = name;
+        }
+
+        public Book() { }
+    }
+}
---
+++ Education/Queue2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Student> studentsDictionary = new Dictionary<string, Student>()
+            {
+                { "Ivan Petrenko", new Student ( "Ivan", "Petrenko", "1", "43", "IF" ) },
+            };
+
+            Queue<Book> books = new Queue<Book>();
+
+            books.Enqueue(new Book { Name = "1" });
+            books.Enqueue(new Book { Name = "2" });
+            books.Enqueue(new Book { Name = "3" });
+
+            foreach (Book b in books)
+            {
+                Console.WriteLine($"Student took book {b.Name}");
+            }
+
+            Book book = books.Dequeue();
+            Console.WriteLine($"Student return book {book.Name}");
+
+            foreach (Book b in books)
+            {
+                Console.WriteLine($"Student return book {b.Name}");
+            }
+        }
+    }
+}

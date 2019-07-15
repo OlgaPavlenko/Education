62452f4af00770caed7fdf6914999e4e636da6e4
Olga Pavlenko <olgaigpavlenko@gmail.com>
2019-07-15 20:42:35 +0300

    added Polymorphysm Human

---
+++ Education/Human/American.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human
+{
+    class American : Human
+    {
+
+        public American(string name)
+        {
+            this.Name = name;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
+
+
---
+++ Education/Human/Ukrainian.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human
+{
+    class Ukrainian : Human
+    {
+        public Ukrainian(string name)
+        {
+            this.Name = name;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine("Доброго дня");
+        }
+    }
+}

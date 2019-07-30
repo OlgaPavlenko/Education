5ebf32e5dd064c9192c558d005fc78090f7bf2b0
Dell <olgaigpavlenko@gmail.com>
2019-07-30 20:07:03 +0300

    added even Tasks

---
+++ Education/Hunter/Hunter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunter
+{
+    public class Hunter
+    {
+        public void Say(object sender, HuntingEventArgs e)
+        {
+            Console.WriteLine($"I saw the {sender.GetType().Name} at: {e.X}, {e.Y}");
+        }
+    }
+}
---
+++ Education/Hunter/Manager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Hunter
+{
+    public delegate void HuntingDelegate(object sender, HuntingEventArgs e);
+
+    public static class Manager
+    {
+        public static void Execute()
+        {
+            Rabbit rabbit = new Rabbit();
+            Hunter hunter = new Hunter();
+
+            rabbit.RabbitEvent += hunter.Say;
+
+            for (int i = 0; i < 10; i++)
+            {
+                rabbit.DoJump();
+                System.Threading.Thread.Sleep(3000);
+            }
+        }
+    }
+}

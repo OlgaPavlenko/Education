344cfce7f699ca6d2600711696507c1e37893ce8
Dell <olgaigpavlenko@gmail.com>
2019-08-31 20:06:11 +0300

    added demo-version of CarRace

---
+++ Education/CarRace/Border.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CarRace
+{
+    public class Border
+    {
+        private int k = 0;
+
+        public Border(int x1, int x2)
+        {
+            this.X1 = x1;
+            this.X2 = x2;
+        }
+
+        public int X1 { get; set; }
+        public int X2 { get; set; }
+
+        public void DownBorderOneStep()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                if (k < 3)
+                {
+                    lock (Program.myLocker)
+                    {
+                        Console.SetCursorPosition(this.X1, i);
+                        Console.Write("||");
+                        Console.SetCursorPosition(this.X2, i);
+                        Console.Write("||");
+                        k++;
+                    }
+                }
+
+                else
+                {
+                    lock (Program.myLocker)
+                    {
+                        Console.SetCursorPosition(this.X1, i);
+                        Console.Write("  ");
+                        Console.SetCursorPosition(this.X2, i);
+                        Console.Write("  ");
+                        k = 0;
+                    }
+                }
+            }
+            k++;
+        }
+
+        public void Separator()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                lock (Program.myLocker)
+                {
+                    Console.SetCursorPosition(28, i);
+                    Console.Write("¦");
+                }
+            }
+        }
+    }
+}
+
---
+++ Education/CarRace/KeyBoard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRace
+{
+    public class KeyBoard
+    {
+        public void ReadMoveMyCar()
+        {
+            while (true)
+            {
+                var selection = Console.ReadKey(true).Key;
+                string selString = Convert.ToString(selection);
+                //Console.Write(selection);
+                switch (selString)
+                {
+                    case "LeftArrow":
+                        Creator.myCar.MakeMyCarInvisible(15, 29);
+                        Creator.myCar.DrawMyCar(4, 29);
+                        break;
+
+                    case "RightArrow":
+                        Creator.myCar.MakeMyCarInvisible(4, 29);
+                        Creator.myCar.DrawMyCar(15, 29);
+                        break;
+
+                    default:
+
+                        break;
+                }
+            }
+        }
+    }
+}
